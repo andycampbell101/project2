f45cfc7134f6bd10a27fbe607b72ea34ad3a35de
jordyn <jordyn.kerrison@live.com.au>
2019-04-30 14:06:28 +1000

    Update site-branding.php

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -16,7 +16,13 @@
 
 		<div class="site-branding-text">
 			<?php if ( is_front_page() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			the_content(
+						sprintf(
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+							get_the_title()
+						)
+					);	
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php endif; ?>
